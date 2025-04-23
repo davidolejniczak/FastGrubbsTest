c9a77dd6a7c5f2910275c4534198a02ac93a0fe2
davidolejniczak <David.olejniczak@icloud.com>
2025-04-23 19:30:39 -0400

    Updated code

--- Timing_grubbs.py
+++ Timing_grubbs.py
@@ -1,8 +1,6 @@
 from grubbstest import run_Grubbs
 import time
 import ast
-import numpy as np
-
 
 def main():
     total1k = 0
@@ -12,39 +10,39 @@ def main():
         with open("Timing/data1k.txt","r") as f:
             file = f.read()
         time.sleep(1)
-        data = np.array(ast.literal_eval(file))
-        start = time.time_ns()
+        data = ast.literal_eval(file)
+        start = time.time()
         run_Grubbs(data, alpha=0.05, use_id_field=True, use_list_output=True)
-        end = time.time_ns()
+        end = time.time()
         total = end - start
         total1k += total
-        print(f"Elasped time for 1k: {total} nanoseconds")
+        print(f"Elasped time for 1k: {total} seconds")
         
         with open("Timing/data10k.txt","r") as f:
             file = f.read()
-        data2 = np.array(ast.literal_eval(file))
+        data2 = ast.literal_eval(file)
         time.sleep(1)
-        start2 = time.time_ns()
+        start2 = time.time()
         run_Grubbs(data2, alpha=0.05, use_id_field=True, use_list_output=True)
-        end2 = time.time_ns()
+        end2 = time.time()
         total2 = end2 - start2
         total10k += total2
-        print(f"Elasped time for 10k: {total2} nanoseconds")
+        print(f"Elasped time for 10k: {total2} seconds")
         
         with open("Timing/data100k.txt","r") as f:
             file = f.read()
         time.sleep(1)
-        data3 = np.array(ast.literal_eval(file))
-        start3 = time.time_ns()
+        data3 = ast.literal_eval(file)
+        start3 = time.time()
         run_Grubbs(data3, alpha=0.05, use_id_field=True, use_list_output=True)
-        end3 = time.time_ns()
+        end3 = time.time()
         total3 = end3 - start3
         total100k += total3
-        print(f"Elasped time for 100k: {total3} nanoseconds")
+        print(f"Elasped time for 100k: {total3} seconds")
         
-    print(f"Elasped average time for 1k: {total1k/30} nanoseconds")
-    print(f"Elasped average time for 10k: {total10k/30} nanoseconds")
-    print(f"Elasped average time for 100k: {total100k/30} nanoseconds")
+    print(f"Elasped average time for 1k: {total1k/30} seconds")
+    print(f"Elasped average time for 10k: {total10k/30} seconds")
+    print(f"Elasped average time for 100k: {total100k/30} seconds")
 
 if __name__ == "__main__":
     main()
