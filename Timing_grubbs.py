389fca9909f76acc16e15db06d8637b350e3ba7e
David Olejniczak <david.olejniczak@icloud.com>
2026-01-25 22:15:56 -0500

    Removed 100k test

--- Timing_grubbs.py
+++ Timing_grubbs.py
@@ -28,21 +28,6 @@ def main():
         total2 = end2 - start2
         total10k += total2
         print(f"Elasped time for 10k: {total2} seconds")
-        
-        with open("Timing/data100k.txt","r") as f:
-            file = f.read()
-        time.sleep(1)
-        data3 = ast.literal_eval(file)
-        start3 = time.time()
-        run_Grubbs(data3, alpha=0.05, use_id_field=True, use_list_output=True)
-        end3 = time.time()
-        total3 = end3 - start3
-        total100k += total3
-        print(f"Elasped time for 100k: {total3} seconds")
-        
-    print(f"Elasped average time for 1k: {total1k/30} seconds")
-    print(f"Elasped average time for 10k: {total10k/30} seconds")
-    print(f"Elasped average time for 100k: {total100k/30} seconds")
 
 if __name__ == "__main__":
     main()
